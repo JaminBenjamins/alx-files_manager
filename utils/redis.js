f4bb0d09f7b70d181c3b3289aef03a3d15cd967a
jaminbenjamins <jaminbenjamins@gmail.com>
2023-12-04 08:06:55 -0500

    Defining redis client class

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+	constructor() {
+		this.client = redis.createClient();
+
+		//Handling exception for creating client instance
+		this.client.on('error', (err) => {
+			console.error(`Redis client error: ${err}`);
+		});
+	}
+	isAlive() {
+		return this.client.isConnected();
+	}
+
+	async get(key) {
+		const getAsync = promisify(this.client.get).bind(this.client);
+		return getAsync(key);
+	}
+
+	async set(key, value, duration) {
+		const setAsync = promisify(this.client.setex).bind(this.client);
+		return setAsync(key, value, duration);
+	}
+
+	async del(key) {
+		const delAsync = promisify(this.client.del).bind(this.client);
+		return delAsync(key);
+	}
+}
+
+//Makin the class accessible globally
+const redisClient = new RedisClient();
+module.exports = redisClient;
